6e2ed5e55e2eb48df1e35efeaa664475c6d157f2
Patrick Bryan <pbryan9@gmail.com>
2023-02-27 19:21:23 -0800

    starting setup

---
+++ client/components/Homepage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Homepage() {
+  return <h1 className="text-3xl text-blue-400">This is the home page</h1>;
+}
---
+++ client/components/index.js
@@ -0,0 +1,2 @@
+export { default as Main } from './Main.jsx';
+export { default as Homepage } from './Homepage.jsx';
