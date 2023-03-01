f7b625189dbcbdfd5cfaa1e8744dad7aed4c23d9
inathan44 <inathan44@yahoo.com>
2023-03-01 18:51:46 -0500

    Created single product GET route

--- server/API/routes/products.js
+++ server/API/routes/products.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
+const { Product } = require('../../DB');
+
+// Get single product route /api/products/:productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    res.json(product);
+  } catch (e) {
+    console.error(chalk.bgRed('BACKEND ISSUE FETCHING PRODUCT'));
+    next(e);
+  }
+});
 
 module.exports = router;
