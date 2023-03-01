513d2aa060637f90002e9be63cf58813dee75fd4
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-01 18:54:43 -0500

    add all prods get route

--- server/API/routes/products.js
+++ server/API/routes/products.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const chalk = require('chalk');
+const { Product } = require('../../DB');
+
+// GET all prods
+router.get('/', async (req, res, next) => {
+  try {
+    const allProds = await Product.findAll();
+    res.json(allProds);
+  } catch (e) {
+    console.error(chalk.bgRed('BACKEND ISSUE FETCHING ALL PRODUCTS'));
+    next(err);
+  }
+});
 
 module.exports = router;
