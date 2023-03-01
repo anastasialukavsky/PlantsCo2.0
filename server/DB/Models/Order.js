b52da3511fcaf161b5616f6f89376c0121e9acde
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-01 10:00:52 -0500

    replace virtual fields with regular fields

--- server/DB/Models/Order.js
+++ server/DB/Models/Order.js
@@ -6,27 +6,22 @@ const Order = db.define('order', {
     type: Sequelize.DECIMAL(10, 2),
     allowNull: false,
     defaultValue: 1,
+    validate: {
+      isEmpty: false,
+    },
   },
   finalPrice: {
-    type: Sequelize.VIRTUAL,
-    get() {
-      const orderItems = this.getOrder_Detail();
-      const total = orderItems.reduce(
-        (acc, orderItem) => (acc += orderItem.totalPrice),
-        0
-      );
-      return total * this.promoRate;
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isEmpty: false,
     },
   },
   totalQty: {
-    type: Sequelize.VIRTUAL,
-    get() {
-      const orderItems = this.getOrder_Detail();
-      const total = orderItems.reduce(
-        (acc, orderItem) => (acc += orderItem.qty),
-        0
-      );
-      return total;
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      isEmpty: false,
     },
   },
 });
