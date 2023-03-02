1c77d572ab91a4e514eadc965918e2cda2d133bf
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-02 16:12:11 -0500

    add single prod edit and delete routes

--- server/API/routes/products.js
+++ server/API/routes/products.js
@@ -20,7 +20,7 @@ router.get('/:productId', async (req, res, next) => {
       include: Tag,
     });
     if (product === null) {
-      return res.status(404).send('product not found!');
+      return res.status(404).send('Product not found!');
     }
     res.json(product);
   } catch (e) {
@@ -39,7 +39,7 @@ router.post('/', async (req, res, next) => {
       defaults: req.body,
     });
     if (!wasCreated) return res.status(409).send('Product already exists');
-    res.status(201).send(newProduct);
+    res.status(201).json(newProduct);
   } catch (e) {
     console.error(chalk.bgRed('BACKEND ISSUE ADDING NEW PROD'));
     next(e);
@@ -47,7 +47,29 @@ router.post('/', async (req, res, next) => {
 });
 
 // PUT (admin only - token auth headers)
+router.put('/:id', async (req, res, next) => {
+  try {
+    const prod = await Product.findByPk(req.params.id);
+    if (!prod) return res.status(404).send('No product to update!');
+    const updatedProd = await prod.update(req.body);
+    res.json(updatedProd);
+  } catch (e) {
+    console.error(chalk.bgRed('BACKEND ISSUE UPDATING PRODUCT'));
+    next(e);
+  }
+});
 
 // DELETE (admin only - token auth headers)
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const prod = await Product.findByPk(req.params.id);
+    if (!prod) return res.status(404).send('No product to delete!');
+    await prod.destroy();
+    res.json(prod);
+  } catch (e) {
+    console.error(chalk.bgRed('BACKEND ISSUE DELETING PRODUCT'));
+    next(e);
+  }
+});
 
 module.exports = router;
