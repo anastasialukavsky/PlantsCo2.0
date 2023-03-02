f3dbc2dd8daf3bbecefe87da27902bb1d51f230d
inathan44 <inathan44@yahoo.com>
2023-03-02 18:07:07 -0500

    Created AllProducts component

---
+++ client/components/AllProducts.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  selectAllProducts,
+  fetchAllProducts,
+} from '../slices/product/productSlice';
+
+const AllProducts = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, [dispatch]);
+
+  const allProducts = useSelector(selectAllProducts);
+  console.log('allProducts', allProducts);
+
+  return (
+    <div className="text-red-800 text-3xl">
+      <h1 className=" text-sm">This is the all products page</h1>
+      {allProducts.slice(0, 5).map((product) => {
+        return (
+          <section key={product.id} className="flex">
+            <h1 className="">{product.name}</h1>
+            <img src={product.imageURL} alt="Picture of plant on a counter" />
+            <p>${product.price}</p>
+          </section>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AllProducts;
