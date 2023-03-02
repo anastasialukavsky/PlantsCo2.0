b40e74d77d3eafc83efcfb82dd5d495da7fa2273
Patrick Bryan <pbryan9@gmail.com>
2023-03-02 15:00:13 -0800

    correct status, remove role setter, add get route

--- server/API/routes/users.js
+++ server/API/routes/users.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const { requireToken, isAdmin } = require('../authMiddleware');
 const { User } = require('../../DB');
-// import requireToken and admin from middleware file
 
 // api/user/userid/cart
 // router.use('/:id/cart', require('./cart'));
 // router.use('/:id/wishlist', require('./wishlist'));
+
+router.get('/', requireToken, isAdmin, async (req, res, next) => {
+  // fetch all users info (ADMIN ONLY)
+  // any includes? payments/shipments? cart? wishlists?
+  try {
+    const users = await User.findAll({
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post('/', async (req, res, next) => {
   try {
@@ -17,13 +30,23 @@ router.post('/', async (req, res, next) => {
       email,
       imageURL: imageURL || 'Default-Avatar.svg',
       password,
-      role: 'user',
     });
     if (newUser.id) {
       res
-        .status(200)
+        .status(201)
         .send({ token: await User.authenticate({ email, password }) });
     }
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.get('/:userId', requireToken, (req, res, next) => {
+  try {
   } catch (err) {
     next(err);
   }
