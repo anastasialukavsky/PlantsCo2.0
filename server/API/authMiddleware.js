2119fb685cac2e86b3f86b53baadb80714b4f3b7
Patrick Bryan <pbryan9@gmail.com>
2023-03-02 14:58:38 -0800

    add next() to middleware

--- server/API/authMiddleware.js
+++ server/API/authMiddleware.js
@@ -22,6 +22,7 @@ async function isAdmin(req, res, next) {
     if (!req.user.isAdmin) {
       return res.status(403).send('Inadequate access rights.');
     }
+    next();
   } catch (err) {
     next(err);
   }
