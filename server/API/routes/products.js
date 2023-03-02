2e2288595b185adbcd150659b8d28825a0ec2670
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-02 10:21:33 -0500

    add 404 status for null on single prod route

--- server/API/routes/products.js
+++ server/API/routes/products.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const chalk = require('chalk');
 const { Product, Tag } = require('../../DB');
 
-// GET all prods
+// GET all prods /api/products
 router.get('/', async (req, res, next) => {
   try {
     const allProds = await Product.findAll({ include: Tag });
@@ -13,17 +13,26 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-// Get single product route /api/products/:productId
+// GET single product route /api/products/:productId
 router.get('/:productId', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId, {
       include: Tag,
     });
+    if (product === null) {
+      return res.status(404).send('Oops! Product not found!');
+    }
     res.json(product);
   } catch (e) {
     console.error(chalk.bgRed('BACKEND ISSUE FETCHING SINGLE PRODUCT'));
     next(e);
   }
 });
+
+// POST (admin only - token auth headers)
+
+// UPDATE (admin only - token auth headers)
+
+// DELETE (admin only - token auth headers)
 
 module.exports = router;
