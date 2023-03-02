b8a80896d067cec78871a45f0bab6aef714fd6b6
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-02 13:08:40 -0500

    add currency and promo routes to index file

--- server/API/index.js
+++ server/API/index.js
@@ -4,6 +4,8 @@ router.use('/products', require('./routes/products'));
 router.use('/users', require('./routes/products'));
 router.use('/auth', require('./routes/auth'));
 router.use('/orders', require('./routes/orders'));
+router.use('/currency', require('./routes/currency'));
+router.use('/promos', require('./routes/promos'));
 
 router.use((req, res, next) => {
   const err = new Error('API ROUTE NOT FOUND!');
