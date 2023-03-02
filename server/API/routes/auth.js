ca44b2f5f9cb88479c4458a1ae44fa10f1dc7132
Patrick Bryan <pbryan9@gmail.com>
2023-03-02 13:50:37 -0800

    add auth routes

--- server/API/routes/auth.js
+++ server/API/routes/auth.js
@@ -1,3 +1,5 @@
+const { User } = require('../../DB');
+const { requireToken, isAdmin } = require('../authMiddleware');
 // express routes that handle post (serve new tokens) and get (verify tokens) requests
 
 // log in routes
@@ -7,3 +9,30 @@
 // create user using these fields (to avoid injection errors)
 
 // GET '/user'
+
+const router = require('express').Router();
+
+/**
+ * INBOUND ROUTE: /api/auth
+ */
+
+router.get('/', requireToken, (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (ex) {
+    next(ex);
+    1;
+  }
+});
+
+router.post('/signin', async (req, res, next) => {
+  try {
+    console.log('req.body:', req.body);
+    const { email, password } = req.body;
+    res.send({ token: await User.authenticate({ email, password }) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
