f255edb8d0e179c1e6aba3197eba9b03db915874
Patrick Bryan <pbryan9@gmail.com>
2023-03-02 13:51:04 -0800

    add initial user signup route (/api/users POST)

--- server/API/routes/users.js
+++ server/API/routes/users.js
@@ -1,5 +1,32 @@
+const router = require('express').Router();
+const { requireToken, isAdmin } = require('../authMiddleware');
+const { User } = require('../../DB');
 // import requireToken and admin from middleware file
 
 // api/user/userid/cart
-router.use('/:id/cart', require('./routes/cart'));
-router.use('/:id/wishlist', require('./routes/wishlist'));
+// router.use('/:id/cart', require('./cart'));
+// router.use('/:id/wishlist', require('./wishlist'));
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, imageURL, password } = req.body;
+
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      imageURL: imageURL || 'Default-Avatar.svg',
+      password,
+      role: 'user',
+    });
+    if (newUser.id) {
+      res
+        .status(200)
+        .send({ token: await User.authenticate({ email, password }) });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
