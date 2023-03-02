dbafc60d23d68ae371e0788c878b8e04e883625e
inathan44 <inathan44@yahoo.com>
2023-03-02 18:11:14 -0500

    Thunk, ER for fetching all products

--- client/slices/product/productSlice.js
+++ client/slices/product/productSlice.js
@@ -1,13 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {};
+const initialState = {
+  products: [],
+  singleProduct: {},
+  error: '',
+  status: '',
+};
+
+export const fetchAllProducts = createAsyncThunk(
+  'products/fetchAll',
+  async () => {
+    const { data } = await axios.get(`/api/products`);
+    return data;
+  }
+);
 
 const productSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
+      state.products = action.payload;
+    });
+  },
 });
 
 export const {} = productSlice.actions;
+
+export const selectAllProducts = (state) => state.products.products;
 
 export default productSlice.reducer;
