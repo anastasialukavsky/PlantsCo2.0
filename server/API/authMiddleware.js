0ef1174c8fc4b54297b9f745a2d43d213daaf039
Patrick Bryan <pbryan9@gmail.com>
2023-03-02 13:48:58 -0800

    add auth middleware functions

--- server/API/authMiddleware.js
+++ server/API/authMiddleware.js
@@ -1,8 +1,31 @@
+const { User } = require('../DB/index');
+
 // takes an auth header and uses it to verify logged in user
 // pull in User model
 // store funcs that act as middleware b/n request and response
 // call next() w/o arg to move onto next piece of middleware
 
 // requireToken (logged in user)
+async function requireToken(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.verifyByToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function isAdmin(req, res, next) {
+  try {
+    if (!req.user.isAdmin) {
+      return res.status(403).send('Inadequate access rights.');
+    }
+  } catch (err) {
+    next(err);
+  }
+}
 
 // isAdmin/role (admin priv) (req.user.isAdmin / req.user.role === ...)
+module.exports = { requireToken, isAdmin };
