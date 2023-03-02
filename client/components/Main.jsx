58c4d7d716b3e8506e46ae47820c0f27059dc801
inathan44 <inathan44@yahoo.com>
2023-03-02 16:43:01 -0500

    Linked store provider

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import store from '../store';
 import { Homepage } from './index';
 
 export default function Main() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
