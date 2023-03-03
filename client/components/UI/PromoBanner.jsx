dac20f5b07bfe7e9e82722f3c78335bc716c9111
inathan44 <inathan44@yahoo.com>
2023-03-03 15:16:11 -0500

    promo banner minus color/font

---
+++ client/components/UI/PromoBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PromoBanner = () => {
+  return (
+    <div className="bg-teal-200 h-8 flex items-center justify-center">
+      <h2 className="">Use code GRACESHOPPER for 10% off your entire order!</h2>
+    </div>
+  );
+};
+
+export default PromoBanner;
