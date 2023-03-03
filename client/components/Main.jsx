85535382a1bcb46823c0dfa6fde0c6faf5c7c3ef
inathan44 <inathan44@yahoo.com>
2023-03-03 02:56:52 -0500

    Temp route for singleprod

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import store from '../store';
-import { Homepage, AllProducts } from './index';
+import { Homepage, AllProducts, SingleProduct } from './index';
 
 export default function Main() {
   return (
@@ -11,6 +11,8 @@ export default function Main() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/products" element={<AllProducts />} />
+          {/* BELOW ROUTE WILL BE CHANGED, MADE FOR STYLING PURPOSES ONLY */}
+          <Route path="/singleproduct" element={<SingleProduct />} />
         </Routes>
       </Provider>
     </div>
