25f055c426fa6fa0a0886a14fe0e51f25db74625
inathan44 <inathan44@yahoo.com>
2023-03-03 18:31:20 -0500

    Import NavBar

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import store from '../store';
-import { Homepage, AllProducts, SingleProduct } from './index';
+import { Homepage, AllProducts, SingleProduct, NavBar } from './index';
 
 export default function Main() {
   return (
-    <div className="font-fraunces">
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/products" element={<AllProducts />} />
-          <Route path="/products/:productId" element={<SingleProduct />} />
-        </Routes>
-      </Provider>
-    </div>
+    <>
+      <div className="font-fraunces text-primary-deep-green">
+        <Provider store={store}>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/products" element={<AllProducts />} />
+            <Route path="/products/:productId" element={<SingleProduct />} />
+          </Routes>
+        </Provider>
+      </div>
+    </>
   );
 }
--- client/components/index.js
+++ client/components/index.js
@@ -2,3 +2,4 @@ export { default as Main } from './Main.jsx';
 export { default as Homepage } from './Homepage.jsx';
 export { default as AllProducts } from './AllProducts.jsx';
 export { default as SingleProduct } from './SingleProduct.jsx';
+export { default as NavBar } from './UI/NavBar.jsx';
