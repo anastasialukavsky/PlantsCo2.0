f064870c1fc8c47d5526cbcb785ad544f0a77e3f
inathan44 <inathan44@yahoo.com>
2023-03-03 17:02:20 -0500

    Data is pulled from db, main style done

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,44 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from './UI/NavBar.jsx';
 import PromoBanner from './UI/PromoBanner.jsx';
 import box from '../../public/assets/box.svg';
-// import samplePlant from '../public/assets/samplePlant.png';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectSingleProduct,
+  fetchSingleProduct,
+} from '../slices/product/productSlice.js';
 
 const SingleProduct = () => {
+  const dispatch = useDispatch();
+  const { productId } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchSingleProduct(productId));
+
+    return () => dispatch(resetStatusError());
+  }, [dispatch, productId]);
+
+  const SingleProduct = useSelector(selectSingleProduct);
+  console.log('SingleProduct', SingleProduct);
+  console.log('SingleProduct tags', SingleProduct?.tags?.tagName?.join(','));
+  console.log(
+    'SingleProduct tags',
+    SingleProduct?.tags?.map(({ tagName }) => tagName).join(', ')
+  );
+
   return (
     <>
       <NavBar />
       <PromoBanner />
-      <main className="font-serif flex h-[80vh] ">
+      <main className="font-serif flex h-[600px] ">
         <section className="flex gap-20 justify-center mt-16 ">
           <div className="">
             <img
               className="h-5/6"
-              src="/assets/product_img/plant1.png"
+              src={`/${SingleProduct.imageURL}`}
               alt="error showing photo"
             />
           </div>
           <div className="w-1/3">
             <header className="text-center text-green-900 text-3xl mb-16">
-              ECHINEVERIA CHROMA
+              {SingleProduct.name}
             </header>
             <div className="flex justify-between border-b-4 pb-2 mb-4">
-              <p>Easy care, low water, outdoor</p>
+              <p>
+                {SingleProduct?.tags?.map(({ tagName }) => tagName).join(', ')}
+              </p>
               <p className="text-2xl">❤️</p>
             </div>
             <p className="text-primary-deep-green text-3xl font-bold mb-4">
-              $39
+              ${SingleProduct.price}
             </p>
             <p className="mb-8 leading-tight">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-              nesciunt dolores qui velit voluptatibus! Illo sed quidem similique
-              aspernatur nemo pariatur ipsum, dolore veniam provident, eos
-              adipisci error cumque cupiditate deserunt necessitatibus
-              excepturi.
+              {SingleProduct.shortDescription}
             </p>
             <div className="border-b-4 pb-4 mb-3">
-              <button className="w-full bg-primary-deep-green text-white py-3 rounded-2xl mx-auto block text-xl">
+              <button className="hover:bg-primary-button-green w-full bg-primary-deep-green text-white py-3 rounded-2xl mx-auto block text-xl hover:transition-all">
                 ADD TO CART
               </button>
             </div>
