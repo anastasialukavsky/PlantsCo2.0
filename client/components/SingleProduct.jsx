0cad2f2ea38cfdc3ad1ad415032b6cc0518af9c4
inathan44 <inathan44@yahoo.com>
2023-03-03 18:30:48 -0500

    Links route to all prod / single prod on click

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import NavBar from './UI/NavBar.jsx';
 import PromoBanner from './UI/PromoBanner.jsx';
 import box from '../../public/assets/box.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   selectSingleProduct,
   fetchSingleProduct,
+  resetStatusError,
 } from '../slices/product/productSlice.js';
 
 const SingleProduct = () => {
@@ -20,16 +20,11 @@ const SingleProduct = () => {
   }, [dispatch, productId]);
 
   const SingleProduct = useSelector(selectSingleProduct);
-  console.log('SingleProduct', SingleProduct);
-  console.log('SingleProduct tags', SingleProduct?.tags?.tagName?.join(','));
-  console.log(
-    'SingleProduct tags',
-    SingleProduct?.tags?.map(({ tagName }) => tagName).join(', ')
-  );
+
+  const handleDescriptionClick = (e) => {};
 
   return (
     <>
-      <NavBar />
       <PromoBanner />
       <main className="font-serif flex h-[600px] ">
         <section className="flex gap-20 justify-center mt-16 ">
@@ -41,21 +36,22 @@ const SingleProduct = () => {
             />
           </div>
           <div className="w-1/3">
-            <header className="text-center text-green-900 text-3xl mb-16">
-              {SingleProduct.name}
-            </header>
+            <div className="flex justify-between">
+              <header className=" text-green-900 text-3xl mb-8">
+                {SingleProduct.name}
+              </header>
+              <p className="text-2xl">❤️</p>
+            </div>
+
             <div className="flex justify-between border-b-4 pb-2 mb-4">
               <p>
                 {SingleProduct?.tags?.map(({ tagName }) => tagName).join(', ')}
               </p>
-              <p className="text-2xl">❤️</p>
             </div>
             <p className="text-primary-deep-green text-3xl font-bold mb-4">
               ${SingleProduct.price}
             </p>
-            <p className="mb-8 leading-tight">
-              {SingleProduct.shortDescription}
-            </p>
+            <p className="mb-8 leading-tight">{SingleProduct.description}</p>
             <div className="border-b-4 pb-4 mb-3">
               <button className="hover:bg-primary-button-green w-full bg-primary-deep-green text-white py-3 rounded-2xl mx-auto block text-xl hover:transition-all">
                 ADD TO CART
--- client/components/UI/NavBar.jsx
+++ client/components/UI/NavBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import searchIcon from '../../../assets/icons8-search.svg';
+import { Link } from 'react-router-dom';
 import searchIcon from '../../../public/assets/search-icon.svg';
 
 const NavBar = () => {
   return (
-    <nav className="border-b-2 border-green-900 flex justify-between font-serif h-20 items-center tracking-tighter text-green-900">
+    <nav className="border-b-2 border-green-900 flex justify-between font-serif h-20 items-center tracking-tighter text-green-900 px-5">
       <div>
-        <h1 className="text-green-900 text-5xl font-bold ml-3">Plants&Co</h1>
+        <h1 className="text-green-900 text-5xl font-bold">Plants&Co</h1>
       </div>
-      <div className="flex gap-10 mr-3">
+      <div className="flex gap-10">
         <div className="flex gap-1 stroke-green-900">
           <img src={searchIcon} alt="magnifying glass" className="w-6" />
           <input
@@ -18,7 +18,9 @@ const NavBar = () => {
           />
         </div>
         <ul className="flex text-2xl gap-10">
-          <li>Shop</li>
+          <Link to={`/products`}>
+            <li>Shop</li>
+          </Link>
           <li>Login</li>
           <li>Cart</li>
         </ul>
