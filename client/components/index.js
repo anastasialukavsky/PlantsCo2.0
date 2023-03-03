cb1f1fe4053dfe2da4c25723022255be644328a8
inathan44 <inathan44@yahoo.com>
2023-03-03 02:56:29 -0500

    Import singleprod comp

--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,4 @@
 export { default as Main } from './Main.jsx';
 export { default as Homepage } from './Homepage.jsx';
 export { default as AllProducts } from './AllProducts.jsx';
+export { default as SingleProduct } from './SingleProduct.jsx';
