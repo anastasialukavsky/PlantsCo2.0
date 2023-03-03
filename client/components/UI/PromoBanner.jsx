e4f51e384c7bc796a677bf3da1e68bcc63529547
inathan44 <inathan44@yahoo.com>
2023-03-03 16:29:02 -0500

    Incorporated custom bg color

--- client/components/UI/PromoBanner.jsx
+++ client/components/UI/PromoBanner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PromoBanner = () => {
   return (
-    <div className="bg-teal-200 h-8 flex items-center justify-center">
+    <div className="bg-primary-promo-banner h-9 flex items-center justify-center text-lg">
       <h2 className="">Use code GRACESHOPPER for 10% off your entire order!</h2>
     </div>
   );
