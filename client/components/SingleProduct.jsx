226673b879063f217ebb5b5b582553ccbd5965d3
inathan44 <inathan44@yahoo.com>
2023-03-03 15:16:36 -0500

    Continued styling

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import NavBar from './UI/NavBar.jsx';
+import PromoBanner from './UI/PromoBanner.jsx';
 // import samplePlant from '../../public/assets/samplePlant.png';
 
 const SingleProduct = () => {
   return (
-    <main className="font-serif flex h-screen">
-      <section className="flex gap-20 justify-center items-center">
-        <div className="">
-          <img
-            className="h-96"
-            src="/assets/samplePlant.png"
-            alt="error showing photo"
-          />
-        </div>
-        <div className="w-1/3">
-          <h1 className="text-center text-green-900 text-5xl mb-20">
-            ECHINEVERIA CHROMA
-          </h1>
-          <div className="flex justify-between">
-            <p>Easy care, low water, outdoor</p>
-            <p>❤️</p>
+    <>
+      <NavBar />
+      <PromoBanner />
+      <main className="font-serif flex h-screen">
+        <section className="flex gap-20 justify-center items-center">
+          <div className="">
+            <img
+              className="h-96"
+              src="/assets/samplePlant.png"
+              alt="error showing photo"
+            />
           </div>
-          <p>$39</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            nesciunt dolores qui velit voluptatibus! Illo sed quidem similique
-            aspernatur nemo pariatur ipsum, dolore veniam provident, eos
-            adipisci error cumque cupiditate deserunt necessitatibus excepturi
-            porro mollitia dicta autem officiis modi, nulla vero dignissimos.
-            Neque error doloremque ipsum officiis officia, dolor et.
-          </p>
-          <button className="w-4/5 bg-green-900 text-white py-3 rounded mx-auto block">
-            ADD TO CART
-          </button>
-        </div>
-      </section>
-    </main>
+          <div className="w-1/3">
+            <header className="text-center text-green-900 text-3xl mb-20">
+              ECHINEVERIA CHROMA
+            </header>
+            <div className="flex justify-between">
+              <p>Easy care, low water, outdoor</p>
+              <p>❤️</p>
+            </div>
+            <p>$39</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              nesciunt dolores qui velit voluptatibus! Illo sed quidem similique
+              aspernatur nemo pariatur ipsum, dolore veniam provident, eos
+              adipisci error cumque cupiditate deserunt necessitatibus excepturi
+              porro mollitia dicta autem officiis modi, nulla vero dignissimos.
+              Neque error doloremque ipsum officiis officia, dolor et.
+            </p>
+            <button className="w-4/5 bg-green-900 text-white py-3 rounded mx-auto block">
+              ADD TO CART
+            </button>
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 
