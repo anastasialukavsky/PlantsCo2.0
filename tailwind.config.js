1b6ead06b2dd788ba764b0c241e35ac8430fe7c9
ruralFluffyDill <stacylukavsky@gmail.com>
2023-03-03 14:09:25 -0500

    add basic pallete and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./public/**/*.{js,jsx,css}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      fraunces: ['Fraunces', 'serif'],
+      raleway: ['Raleway', 'sans-serif'],
+      gloock: ['Gloock', 'serif'],
+      outfit: ['Outfit', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary_deep_green: '#365314',
+        'primary-promo-banner': '#a7bfb4',
+        'primary-button-green': '#283F3B',
+        'primary-offwhite': '#f4f4f4',
+        'primary-bright-white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
