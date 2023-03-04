188a321ebada4118db79c1ac7b61f9a35a3059bb
Patrick Bryan <pbryan9@gmail.com>
2023-03-04 07:49:28 -0800

    refactor wishlist spec to make use of outsourced dummy info

--- test/wishlistRoute.spec.js
+++ test/wishlistRoute.spec.js
@@ -2,71 +2,38 @@ process.env.NODE_ENV = 'test';
 
 const sequelize = require('sequelize');
 const { User, Product } = require('../server/DB');
+const { userSetup, productSetup } = require('./dummies');
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const server = require('../server/server');
 
-// const seed = require('../seed');
-
 chai.use(chaiHttp);
-
-const adminDummy = {
-  firstName: 'Travus',
-  lastName: 'Seater',
-  email: 'tseater1@nature.com',
-  imageURL: 'http://dummyimage.com/100x100.png/5fa2dd/ffffff',
-  password: 'Q4dgqiiH',
-  isAdmin: true,
-  role: 'user',
-  currencyId: 1,
-};
-
-const userDummy = {
-  firstName: 'Joellyn',
-  lastName: 'Moreno',
-  email: 'jmoreno2@baidu.com',
-  imageURL: 'http://dummyimage.com/100x100.png/ff4444/ffffff',
-  password: 'LVuVthfVZDX',
-  isAdmin: false,
-  role: 'user',
-  currencyId: 4,
-};
 
 let regularUser;
 let regularToken;
 let adminUser;
 let adminToken;
 let seededProducts;
+let createdWishlist;
 
 describe('Wishlists', () => {
   beforeEach(async () => {
-    // empty the Users table
-    await User.destroy({ truncate: true, cascade: true });
+    if (!adminToken || !regularToken) {
+      const temp = await userSetup();
+      regularUser = temp.regularUser;
+      regularToken = temp.regularToken;
+      adminUser = temp.adminUser;
+      adminToken = temp.adminToken;
+    }
+    if (!seededProducts) {
+      seededProducts = await productSetup();
+    }
+  });
 
-    // add 2 users (1 regular / 1 admin)
-    regularUser = await User.create(userDummy);
-    adminUser = await User.create(adminDummy);
-    const seedUsers = [regularUser, adminUser];
-
-    // log in the regular user & store token
-    let res = await chai
-      .request(server)
-      .post('/api/auth/login')
-      .type('json')
-      .send({ email: userDummy.email, password: userDummy.password });
-
-    regularToken = res.body.token;
-
-    // log in the admin user & store token
-    res = await chai
-      .request(server)
-      .post('/api/auth/login')
-      .type('json')
-      .send({ email: adminDummy.email, password: adminDummy.password });
-
-    adminToken = res.body.token;
+  describe('POST/users/:userId/wishlists', () => {
+    it('Creates a new wishlist given correct token,', async () => {});
   });
 
   describe('GET /users/:userId/wishlists', () => {
