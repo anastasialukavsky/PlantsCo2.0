5abd107df35983531cabbcb0f86a8902c699e9c1
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-04 14:34:19 -0500

    add ternary for navbar if logged in or not

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import store from '../store';
-import { Homepage, AllProducts, SingleProduct, NavBar, Login } from './index';
+
+import {
+  Homepage,
+  AllProducts,
+  SingleProduct,
+  NavBar,
+  Login,
+  LoggedInNavBar,
+} from './index';
+import { selectAuth } from '../slices/users/authSlice';
 
 export default function Main() {
+  const { status } = useSelector(selectAuth);
+  const { auth } = useSelector(selectAuth);
+
   return (
     <>
       <div className="font-fraunces text-primary-deep-green">
-        <Provider store={store}>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/products" element={<AllProducts />} />
-            <Route path="/products/:productId" element={<SingleProduct />} />
-          </Routes>
-        </Provider>
+        {status ? <LoggedInNavBar auth={auth} /> : <NavBar />}
+
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:productId" element={<SingleProduct />} />
+        </Routes>
       </div>
     </>
   );
