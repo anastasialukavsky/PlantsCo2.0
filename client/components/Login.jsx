c98a993bfcd2c48a1eba36542eee70c11ac05762
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-04 10:49:53 -0500

    add login component and import across files

---
+++ client/components/Login.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { resetStatus, fetchCart } from '../slices/users/userSlice';
+
+const Login = () => {};
+
+export default Login;
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import store from '../store';
-import { Homepage, AllProducts, SingleProduct, NavBar } from './index';
+import { Homepage, AllProducts, SingleProduct, NavBar, Login } from './index';
 
 export default function Main() {
   return (
@@ -12,6 +12,7 @@ export default function Main() {
           <NavBar />
           <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/products" element={<AllProducts />} />
             <Route path="/products/:productId" element={<SingleProduct />} />
           </Routes>
