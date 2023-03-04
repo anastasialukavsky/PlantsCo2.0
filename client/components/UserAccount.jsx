f2d17c7d2fe66a65bd95115ef4779e19983d0b05
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-04 15:56:41 -0500

    add logout functionality

--- client/components/UserAccount.jsx
+++ client/components/UserAccount.jsx
@@ -3,6 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logOut } from '../slices/users/authSlice';
 
-const UserAccount = () => {};
+const UserAccount = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const attemptLogOut = async () => {
+    await dispatch(logOut());
+    navigate('/');
+  };
+
+  return (
+    <>
+      <h1>ACCOUNT</h1>
+      <button onClick={attemptLogOut}>Log Out</button>
+    </>
+  );
+};
 
 export default UserAccount;
