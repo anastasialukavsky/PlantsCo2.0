a6c61e6c1b383937ab7bffedef7280dd47e522e3
inathan44 <inathan44@yahoo.com>
2023-03-04 16:17:28 -0500

    ATC button at bottom of pic

--- client/components/ProductCard.jsx
+++ client/components/ProductCard.jsx
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom';
 const ProductCard = (props) => {
   const { product } = props;
   return (
-    <div key={product.id} className="relative group">
+    <div key={product.id} className="group">
       <Link to={`/products/${product.id}`}>
-        <img
-          src={product.imageURL}
-          alt="Picture of plant on a counter"
-          className="w-full relative group"
-        />
+        <div className="relative">
+          <img
+            src={`/${product.imageURL}`}
+            alt="Picture of plant on a counter"
+            className="w-full relative group"
+          />
+          <button className="bg-primary-deep-green w-full text-white absolute bottom-0  py-1 invisible group-hover:visible mx-auto opacity-60 transition duration-100 ease-in-out hover:opacity-100">
+            Add To Cart
+          </button>
+        </div>
         <p className="text-2xl mb-1">{product.name}</p>
         <p className="text-sm">${product.price}</p>
       </Link>
-      <button className="bg-primary-deep-green w-full text-white absolute bottom-14  py-1 invisible group-hover:visible mx-auto opacity-60 transition duration-100 ease-in-out hover:opacity-100">
-        Add To Cart
-      </button>
       {/* <button className="bg-primary-deep-green w-full text-white absolute bottom-14 rounded-xl py-1 invisible group-hover:visible mx-auto opacity-60 transition duration-100 ease-in-out hover:opacity-100">
                   Add To Cart
                 </button> */}
