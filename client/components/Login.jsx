89f16273500c7bcb9e989105dba203af81ba4bfd
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-04 19:02:24 -0500

    update login style

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -24,60 +24,64 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full max-w-xs">
-      <section className="flex gap-20 justify-center mt-16 ">
-        <form onSubmit={onSubmit}>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:shadow-outline"
-              id="email"
-              type="text"
-              placeholder="email"
-              value={email}
-              onChange={(evt) => setEmail(evt.target.value)}
-              name="email"
-            />
-          </div>
+    <div className="bg-cover bg-[url('/assets/bg_img/login_signin_page.jpg')]">
+      <div className="w-full max-w-sm m-auto">
+        <h2 className="text-center text-4xl font-bold">Log In</h2>
+        <section className="flex gap-20 justify-center mt-16 ">
+          <form onSubmit={onSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-96 py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:shadow-outline"
+                id="email"
+                type="text"
+                placeholder="email"
+                value={email}
+                onChange={(evt) => setEmail(evt.target.value)}
+                name="email"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-3 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="   ************"
-              value={password}
-              onChange={(evt) => setPassword(evt.target.value)}
-              name="password"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            >
-              Log In
-            </button>
-            {/* <a
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border rounded w-96 py-3 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="   ************"
+                value={password}
+                onChange={(evt) => setPassword(evt.target.value)}
+                name="password"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="hover:bg-primary-button-green w-full bg-primary-deep-green text-white py-2 rounded-2xl mx-auto block text-xl hover:transition-all"
+                // className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              >
+                Log In
+              </button>
+              {/* <a
               className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
               href="#"
             >
               Forgot Password?
             </a> */}
-          </div>
-        </form>
-      </section>
+            </div>
+          </form>
+        </section>
+      </div>
     </div>
   );
 };
