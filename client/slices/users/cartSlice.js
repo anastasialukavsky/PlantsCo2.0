0769a07145e96eefcaf88756098fa6d409231220
Patrick Bryan <pbryan9@gmail.com>
2023-03-04 16:10:43 -0800

    add cartSlice / add to store

---
+++ client/slices/users/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    cart: [],
+  },
+  reducers: {},
+});
+
+export const {} = cart.actions;
+
+export default cart.reducer;
