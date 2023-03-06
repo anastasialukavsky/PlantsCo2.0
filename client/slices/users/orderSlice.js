e496a4d4795a9b6f53213e0ac83409c850ff5d2b
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-06 09:09:03 -0500

    update orderslice

--- client/slices/users/orderSlice.js
+++ client/slices/users/orderSlice.js
@@ -1,11 +1,81 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+export const fetchAllOrders = createAsyncThunk(
+  'fetchAllOrders',
+  async (x, { rejectWithValue }) => {
+    try {
+      let { data } = await axios.get(`/api/orders}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      return data;
+    } catch (err) {
+      console.log('axios error getting all orders');
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const fetchUserOrders = createAsyncThunk(
+  'fetchUserOrders',
+  async ({ id, token }, { rejectWithValue }) => {
+    try {
+      let { data } = await axios.get(`/api/users/${id}/orders`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      return data;
+    } catch (err) {
+      console.log('axios error getting all orders');
+      return rejectWithValue(err);
+    }
+  }
+);
+
 const orderSlice = createSlice({
   name: 'orders',
-  initialState: {},
-  reducers: {},
-  extraReducers: (builder) => {},
+  initialState: {
+    orders: [],
+    order: {},
+    status: '',
+    error: '',
+  },
+  reducers: {
+    resetStatus: (state) => {
+      state.status = '';
+      state.error = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllOrders.fulfilled, (state, { payload }) => {
+        state.orders = payload;
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchAllOrders.pending, (state, { payload }) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllOrders.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      })
+      .addCase(fetchUserOrders.fulfilled, (state, { payload }) => {
+        state.order = payload;
+        state.status = 'success';
+        state.error = '';
+      })
+      .addCase(fetchUserOrders.pending, (state, { payload }) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUserOrders.rejected, (state, { payload }) => {
+        state.status = 'failed';
+        state.error = payload.message;
+      });
+  },
 });
 
 export const {} = orderSlice.actions;
