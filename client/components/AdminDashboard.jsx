61f3116833239b895e2469085c65576319fd9299
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-07 10:17:10 -0500

    add new background image

--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const AdminDashboard = () => {};
+const AdminDashboard = () => {
+  return (
+    <div className="bg-cover bg-center h-[calc(100vh_-_5rem)] bg-[url('/assets/bg_img/admin.jpg')]">
+      <div>ADMIN DASH</div>;
+    </div>
+  );
+};
 
 export default AdminDashboard;
