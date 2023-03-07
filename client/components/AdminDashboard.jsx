ad71991a84e278bc0d1ec7b920ec5b070ff0b8a6
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-07 10:25:29 -0500

    add list of potential sections

--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const AdminDashboard = () => {
   return (
     <div className="bg-cover bg-center h-[calc(100vh_-_5rem)] bg-[url('/assets/bg_img/admin.jpg')]">
-      <div>ADMIN DASH</div>;
+      <div>ADMIN DASH</div>
+      <p>Edit Products</p>
+      <p>Analytics</p>
+      <p>User Controls</p>
+      <p>Plants&Co Shops</p>
     </div>
   );
 };
