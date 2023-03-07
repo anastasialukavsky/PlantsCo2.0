5dda6c47e6acfb2d980846f13afabda36dcdbc1b
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-06 20:18:48 -0500

    create order details page

--- client/components/OrderHistoryDetails.jsx
+++ client/components/OrderHistoryDetails.jsx
@@ -6,21 +6,26 @@ import {
   selectOrders,
   resetStatus,
   fetchOrderDetails,
+  fetchUserOrders,
 } from '../slices/users/orderSlice';
 
 const OrderHistoryDetails = () => {
   const dispatch = useDispatch();
   const params = useParams();
 
-  const [details, setDetails] = useState({});
+  const [details, setDetails] = useState([]);
+  const [userOrder, setUserOrder] = useState({});
+  let addressArr;
+  let date;
 
   const { auth, token } = useSelector(selectAuth);
-  const { orderDetails } = useSelector(selectOrders);
+  const { orderDetails, order } = useSelector(selectOrders);
 
   const userId = auth.id;
   const orderId = +params.orderId;
 
   useEffect(() => {
+    dispatch(fetchUserOrders({ userId, token }));
     dispatch(fetchOrderDetails({ userId, orderId, token }));
 
     return () => {
@@ -30,63 +35,133 @@ const OrderHistoryDetails = () => {
 
   useEffect(() => {
     setDetails(orderDetails || []);
-  }, [details]);
+    const specificOrder = order.filter((order) => order.id === orderId)[0];
+    setUserOrder(specificOrder || {});
+  }, [orderDetails, auth]);
+
+  if (details.length >= 1) {
+    addressArr = details[0].address.split('\n');
+    date = userOrder.createdAt.slice(0, 10);
+  }
+
+  if (details.length < 1)
+    return (
+      <div className="bg-cover bg-center h-[calc(100vh_-_5rem)] bg-[url('/assets/bg_img/cart.jpg')]">
+        <div className="flex flex-col gap-10 w-full max-w-xl absolute top-30 left-10 pt-16">
+          <p className="text-center text-4xl font-extrabold text-primary-deep-green">
+            No Orders!
+          </p>
+          <button className="inline-block align-baseline font-bold text-sm hover:text-primary-promo-banner py-1">
+            <Link to="/account">Back</Link>
+          </button>
+        </div>
+        <div className="pt-50 m-auto"></div>
+      </div>
+    );
 
   return (
     <div className="bg-cover bg-center h-[calc(100vh_-_5rem)] bg-[url('/assets/bg_img/cart.jpg')]">
-      <div className="flex flex-col gap-10 w-3/4 absolute top-30 left-10 pt-16">
+      <div className="flex flex-col gap-10 w-full max-w-xl absolute top-30 left-10 pt-16">
         <p className="text-center text-4xl font-extrabold text-primary-deep-green">
           Previous Orders
         </p>
 
-        <div className="relative overflow-x-auto">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="flex flex-col gap-5 relative overflow-x-auto text-primary-deep-green">
+          <div>
+            <p>DATE: {date}</p>
+            <p>ORDER ID: {orderId}</p>
+            <p>ITEMS: {userOrder.totalQty}</p>
+          </div>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 bg-white rounded-xl">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 rounded-xl">
               <tr>
                 <th scope="col" className="px-6 py-3">
-                  Order Id
+                  Product Name
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Date of Order
+                <th scope="col" className="px-6 py-3 text-center">
+                  Qty
                 </th>
-                <th scope="col" className="px-6 py-3">
-                  Item Qty
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Total
+                <th scope="col" className="px-6 py-3 text-right">
+                  Subtotal
                 </th>
               </tr>
             </thead>
             <tbody>
-              {/* {userOrders && userOrders.length
-                ? userOrders.map((order) => {
+              {details && details.length
+                ? details.map((item) => {
                     return (
-                      <tr
-                        onClick={() => orderDetails(order.id)}
-                        key={order.id}
-                        className="cursor-pointer hover:text-primary-promo-banner"
-                      >
+                      <tr key={item.productId}>
                         <th scope="col" className="px-6 py-3">
-                          {order.id}
+                          {item.productName}
                         </th>
-                        <th scope="col" className="px-6 py-3">
-                          {order.createdAt.slice(0, 10)}
+
+                        <th scope="col" className="px-6 py-3 text-center">
+                          {item.qty}
                         </th>
-                        <th scope="col" className="px-6 py-3">
-                          {order.totalQty}
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          {order.finalPrice}
+                        <th scope="col" className="px-6 py-3 text-right">
+                          ${item.totalPrice.toFixed(2)}
                         </th>
                       </tr>
                     );
                   })
-                : 'No past orders!'} */}
+                : 'No past orders!'}
             </tbody>
+            <tfoot>
+              <tr>
+                <th className="text-md px-6 pt-3">Shipping</th>
+                <th></th>
+                <th className="text-right text-md px-6 pt-3 ">Free</th>
+              </tr>
+              <tr>
+                <th className="text-md px-6 pt-3">Discount</th>
+                <th></th>
+                <th className="text-right text-md px-6 pt-3 ">
+                  -${(details[0].totalPrice * userOrder.promoRate).toFixed(2)}
+                </th>
+              </tr>
+              <tr>
+                <th className="text-md px-6 py-3">Total</th>
+                <th></th>
+                <th className="text-right text-md px-6 py-3">
+                  ${userOrder.finalPrice}
+                </th>
+              </tr>
+            </tfoot>
           </table>
+          <div className="flex flex-row text-primary-deep-green">
+            <div className="w-1/2">
+              <p>PAYMENT METHOD</p>
+              <p>
+                {details[0].paymentMethod === 'cc'
+                  ? 'Credit / debit card'
+                  : details[0].paymentMethod}
+              </p>
+            </div>
+            <div className="w-1/2">
+              <p>SHIPPING TO</p>
+              {addressArr.length === 4 ? (
+                <>
+                  <p>{details[0].userName}</p>
+                  <p>{addressArr[0]}</p>
+                  <p>
+                    {addressArr[1]}, {addressArr[2]} {addressArr[3]}
+                  </p>
+                </>
+              ) : (
+                <>
+                  <p>{details[0].userName}</p>
+                  <p>{addressArr[0]}</p>
+                  <p>{addressArr[2]}</p>
+                  <p>
+                    {addressArr[3]}, {addressArr[4]} {addressArr[5]}
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
         </div>
         <div className="pt-50 m-auto">
-          <button className="inline-block align-baseline font-bold text-sm hover:text-primary-promo-banner py-1">
+          <button className="align-baseline align-text-left font-bold text-sm hover:text-primary-promo-banner py-1">
             <Link to="/account/orderhistory">Back</Link>
           </button>
         </div>
