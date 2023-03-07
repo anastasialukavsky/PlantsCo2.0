244d1075f2d9171b8be28eff63a74ed1d016217d
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-07 17:55:00 -0500

    update admin dash styling, add links

--- client/components/AdminDashboard.jsx
+++ client/components/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
   selectAuth,
   resetStatus as resetAuthStatus,
@@ -33,10 +34,10 @@ const AdminDashboard = () => {
   const [promoTable, setPromoTable] = useState('collapse');
 
   const inactiveButtonClass =
-    'hover:bg-green-900 hover:text-primary-bright-white p-5 rounded-r-full mr-5';
+    'hover:bg-green-900 hover:text-primary-bright-white pl-5 p-3 rounded-r-full mr-5';
 
   const activeButtonClass =
-    'bg-green-900 text-primary-bright-white p-5 rounded-r-full mr-5';
+    'bg-green-900 text-primary-bright-white pl-5 p-3 rounded-r-full mr-5';
 
   const [adminButtonStatus, setAdminButtonStatus] = useState(activeButtonClass);
   const [prodButtonStatus, setProdButtonStatus] = useState(inactiveButtonClass);
@@ -83,7 +84,7 @@ const AdminDashboard = () => {
                 {'ADMIN DASHBOARD'}
               </button>
             </div>
-            <div className="flex flex-col">
+            <div>
               <div className={prodButtonStatus}>
                 <button
                   onClick={() => {
@@ -96,7 +97,7 @@ const AdminDashboard = () => {
                     setUserButtonStatus(inactiveButtonClass);
                     setPromoButtonStatus(inactiveButtonClass);
                   }}
-                  className=""
+                  className="flex flex-row"
                 >
                   {'PRODUCTS'}
                 </button>
@@ -140,6 +141,9 @@ const AdminDashboard = () => {
               </div>
             </div>
           </div>
+          <button className="text-left pl-5 font-bold text-sm hover:text-primary-promo-banner py-1">
+            <Link to={'/account'}>Back</Link>
+          </button>
         </aside>
 
         <div className="p-4 w-3/4  h-[calc(100vh_-_5rem)] overflow-auto">
@@ -212,6 +216,9 @@ const AdminDashboard = () => {
                       <th scope="col" className="px-6 py-3 top-0 sticky">
                         {'QTY'}
                       </th>
+                      <th scope="col" className="px-6 py-3 top-0 sticky">
+                        {'ACTIONS'}
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -234,6 +241,13 @@ const AdminDashboard = () => {
                               </th>
                               <th scope="col" className="px-6 py-3">
                                 {product.qty}
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                <Link
+                                  to={`/account/admin/editproduct/${product.id}`}
+                                >
+                                  {'Edit / Delete'}
+                                </Link>
                               </th>
                             </tr>
                           );
@@ -294,6 +308,16 @@ const AdminDashboard = () => {
               <div id="admin" className={adminTable}>
                 <p>{`Howdy, ${auth.firstName}!`}</p>
                 <p>{"You're an admin... Nice!"}</p>
+                <div>
+                  <span>
+                    <button className="py-3 px-5 mr-2 m-5 text-center text-text-primary-deep-green-900 bg-white rounded-lg border hover:bg-gray-100">
+                      Add Product
+                    </button>
+                    <button className="py-3 px-5 mr-2 m-5 text-center text-text-primary-deep-green-900 bg-white rounded-lg border hover:bg-gray-100">
+                      Add Promo
+                    </button>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
