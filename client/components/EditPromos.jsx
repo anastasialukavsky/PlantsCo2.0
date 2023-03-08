cb8fa0246d19617d4ff185493cf6876314e421c5
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-08 01:34:43 -0500

    add edit promo feature

---
+++ client/components/EditPromos.jsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import {
+  selectPromos,
+  fetchSinglePromo,
+  resetPromoStatus,
+  editPromo,
+  deletePromo,
+} from '../slices/product/promoSlice';
+import {
+  selectAuth,
+  resetStatus as resetAuthStatus,
+} from '../slices/users/authSlice';
+
+const EditProduct = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { promoId } = useParams();
+  const id = promoId;
+  const { promo } = useSelector(selectPromos);
+  const { auth, token } = useSelector(selectAuth);
+
+  const [name, setName] = useState('');
+  const [discountRate, setDiscountRate] = useState('');
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    dispatch(fetchSinglePromo({ promoId, token }));
+
+    return () => {
+      resetPromoStatus();
+      resetAuthStatus();
+    };
+  }, [promoId]);
+
+  useEffect(() => {
+    setName(promo.name || '');
+    setDiscountRate(promo.discountRate || '');
+    setStatus(promo.status || '');
+  }, [promo]);
+
+  const updatePromo = (evt) => {
+    evt.preventDefault();
+    const updates = { name, discountRate, status };
+    console.log('updates:', updates);
+
+    dispatch(editPromo({ promoId, token, updates }));
+    dispatch(fetchSinglePromo({ id, token }));
+  };
+
+  const deletePromo = async () => {
+    await dispatch(deleteSinglePromo({ promoId, token }));
+    if (!promo.name) navigate('/account/admin');
+  };
+
+  return (
+    <div className="bg-cover bg-center h-[calc(100vh_-_5rem)] bg-[url('/assets/bg_img/admin.jpg')]">
+      <div className="flex flex-row">
+        <aside
+          id="default-sidebar"
+          className=" w-1/4 h-[calc(100vh_-_5rem)] transition-transform -translate-x-full sm:translate-x-0 flex flex-col gap-5"
+          aria-label="Sidebar"
+        >
+          <div className="flex flex-col pt-5 gap-3">
+            <div className="hover:bg-green-900 hover:text-primary-bright-white pl-5 p-3 rounded-r-full mr-5">
+              <button className="text-left">
+                <Link to={'/account/admin'}>ADMIN DASHBOARD</Link>
+              </button>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="bg-green-900 text-primary-bright-white pl-5 p-3 rounded-r-full mr-5">
+                <button className="flex flex-row">
+                  <Link to={'/account/admin/products'}>PRODUCTS/EDIT</Link>
+                </button>
+              </div>
+              <div className="hover:bg-green-900 hover:text-primary-bright-white pl-5 p-3 rounded-r-full mr-5">
+                <button className="flex flex-row">
+                  <Link to={'/account/admin/addproduct'}>ADD NEW PRODUCT</Link>
+                </button>
+              </div>
+              <div className="hover:bg-green-900 hover:text-primary-bright-white pl-5 p-3 rounded-r-full mr-5">
+                <button className="text-left">
+                  <Link to={'/account/admin/promos'}>PROMOCODES</Link>
+                </button>
+              </div>
+              <div className="hover:bg-green-900 hover:text-primary-bright-white pl-5 p-3 rounded-r-full mr-5">
+                <button className="text-left">
+                  <Link to={'/account/admin/users'}>USER MANAGEMENT</Link>
+                </button>
+              </div>
+            </div>
+          </div>
+          <button className="text-left pl-5 font-bold text-sm hover:text-primary-promo-banner py-1">
+            <Link to={'/account'}>Back</Link>
+          </button>
+        </aside>
+        <div className="p-4 w-3/4 h-[calc(100vh_-_5rem)] overflow-auto">
+          <section className="flex flex-col w-full">
+            <form className="pl-10 pr-10" onSubmit={updatePromo}>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                  <label
+                    className="block uppercase tracking-wide text-primary-deep-green text-xs font-bold mb-2"
+                    htmlFor="grid-name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4  leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-name"
+                    type="text"
+                    value={name}
+                    onChange={(evt) => setName(evt.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                  <label
+                    className="block uppercase tracking-wide text-primary-deep-green text-xs font-bold mb-2"
+                    htmlFor="grid-first-qty"
+                  >
+                    PROMO RATE
+                  </label>
+                  <input
+                    className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-qty"
+                    type="number"
+                    min={0}
+                    max={1}
+                    step={0.05}
+                    value={discountRate}
+                    onChange={(evt) => setDiscountRate(evt.target.value)}
+                  />
+                </div>
+                <div className="w-full md:w-1/2 px-3">
+                  <label
+                    className="block uppercase tracking-wide text-primary-deep-green text-xs font-bold mb-2"
+                    htmlFor="grid-price"
+                  >
+                    STATUS
+                  </label>
+                  <select
+                    className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-price"
+                    type="text"
+                    value={status}
+                    onChange={(evt) => setStatus(evt.target.value)}
+                  >
+                    <option value="" disabled>
+                      <em>select status</em>
+                    </option>
+                    <option value={true}>true</option>
+                    <option value={false}>false</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex flex-col items-center justify-between">
+                <button
+                  type="submit"
+                  className="hover:bg-primary-button-hover w-full bg-primary-deep-green text-white py-2 rounded-lg mx-auto block text-xl hover:transition-all mt-5"
+                >
+                  Save
+                </button>
+              </div>
+              <div className="flex justify-center"></div>
+            </form>
+            <button
+              className="text-red-600 w-full py-2 rounded-lg block text-sm hover:text-primary-promo-banner mt-5"
+              onClick={deletePromo}
+            >
+              Delete
+            </button>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditProduct;
