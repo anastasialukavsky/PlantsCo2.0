b9de784fd3ea64d90d9e917cbda75f6f786873b4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-29 20:56:59 -0400

    restructured navbar

--- client/components/UI/NavBar.jsx
+++ client/components/UI/NavBar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import searchIcon from '../../../public/assets/search-icon.svg';
 import { useDispatch, useSelector } from 'react-redux';
+import triangle from '../../../public/assets/line.svg'
+
 import {
   selectSearchedItems,
   adjustSearchBy,
@@ -28,32 +30,35 @@ const NavBar = (props) => {
 
   return (
     <header>
-      <nav className="border-b-2 border-green-900 flex justify-between font-serif h-20 items-center tracking-tighter text-green-900 px-5">
+      <nav className="flex h-20 w-screen flex-col pt-2 items-center justify-around tracking-tighter text-green-gray relative z-50">
         <div>
           <Link to={'/'}>
-            <h1 className="text-green-900 text-3xl xs:text-5xl font-bold">
-              Plants&Co
-            </h1>
+            <h1 className="font-tabac text-5xl md:text-[3vw]">plants&co</h1>
           </Link>
         </div>
-        <div className="hidden lg:flex gap-10">
-          <div className="flex gap-1 stroke-green-900">
-            <button onClick={handleSearch}>
-              <img src={searchIcon} alt="magnifying glass" className="w-6" />
-            </button>
-            <form onSubmit={handleSearch}>
-              <input
-                type="text"
-                placeholder="succulent..."
-                className="border-2 border-green-700 rounded-full pl-3"
-                value={searchTerm}
-                onChange={(e) => dispatch(adjustSearchBy(e.target.value))}
-              />
-            </form>
-          </div>
-          <ul className="flex text-2xl gap-10">
+
+        <div className="hidden gap-10 md:flex">
+          {/**
+      <div className="flex gap-1 stroke-green-900">
+      <button onClick={handleSearch}>
+      <img src={searchIcon} alt="magnifying glass" className="w-6" />
+      </button>
+      <form onSubmit={handleSearch}>
+      <input
+      type="text"
+      placeholder="succulent..."
+      className="rounded-full border-2 border-green-700 pl-3"
+      value={searchTerm}
+      onChange={(e) => dispatch(adjustSearchBy(e.target.value))}
+      />
+      </form>
+      </div>
+    */}
+
+    
+          <ul className="flex gap-10 font-outfit md:text-[1.9vw] lg:text-[1.3vw]">
             <Link to={`/products`}>
-              <li onClick={() => dispatch(adjustFilter(''))}>Shop</li>
+              <li onClick={() => dispatch(adjustFilter(''))}>SHOP</li>
             </Link>
             {auth.firstName ? (
               <Link to={'/account'}>
@@ -61,16 +66,23 @@ const NavBar = (props) => {
               </Link>
             ) : (
               <Link to={'/login'}>
-                <li>Login</li>
+                <li>LOGIN</li>
               </Link>
             )}
             <Link to="/cart">
-              <li>Cart</li>
+              <li>CART</li>
             </Link>
           </ul>
         </div>
+
+
+        {/**decorated navbar border */}
+        <div className="relative flex w-[90vw]">
+          <img src={triangle} alt="" className="absolute w-full " />
+        </div>
+
         <button
-          className="lg:hidden  z-30"
+          className="z-30  md:hidden"
           onClick={() => setExpand((prev) => !prev)}
         >
           <img src={menu} alt="dropdown menu icon" className=" w-10" />
