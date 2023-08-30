5e80cefbd75653e3f112b17c22032b2d9d53b9b2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-30 16:09:41 -0400

    change layout&amout of prods rendered per breakpt

--- client/components/SimilarProducts.jsx
+++ client/components/SimilarProducts.jsx
@@ -20,27 +20,38 @@ const SimilarProducts = () => {
   };
 
   return (
-    <section className="mx-auto mb-12 h-96 w-screen max-w-6xl">
-      <h2 className="my-4 ml-4 text-2xl">You might also like</h2>
+    <section className="mx-auto  mt-[9%] pb-[5%] min-h-96 w-screen max-w-6xl">
+      <h2 className="my-4 ml-4 text-center font-outfit text-[2vw]">
+        YOU MIGHT ALSO LIKE
+      </h2>
       <div className="flex md:items-center">
         <button
-          className="block h-full min-w-[52px] max-w-sm"
+          className=" flex h-full min-w-[52px] max-w-sm items-center justify-center"
           onClick={() => handlePageChange('previous')}
         >
-          <img src={leftArrow} alt="left arrow icon" className="w-12" />
+          <img src={leftArrow} alt="left arrow icon" className="w-8" />
         </button>
-        <div className="mx-12 grid grid-cols-1 justify-items-center gap-x-8 gap-y-8 md:grid-cols-2 lg:grid-cols-4 ">
-          {similarProducts
-            ?.slice(similarPage, similarPage + 4)
-            .map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+
+        <div className=" mx-auto grid grid-cols-1 justify-items-center gap-x-8 gap-y-8 md:grid-cols-2 lg:grid-cols-4 ">
+          {similarProducts && window.innerWidth <= 768
+            ? similarProducts
+                .slice(similarPage, similarPage + 2)
+                .map((product) => {
+                  console.log(window.innerWidth);
+                  return <ProductCard key={product.id} product={product} />;
+                })
+            : similarProducts
+                .slice(similarPage, similarPage + 4)
+                .map((product) => {
+                  console.log(window.innerWidth);
+                  return <ProductCard key={product.id} product={product} />;
+                })}
         </div>
         <button
-          className="block h-full min-w-[52px] max-w-sm"
+          className=" flex h-full min-w-[52px] max-w-sm items-center justify-center"
           onClick={() => handlePageChange(['next', similarProducts.length])}
         >
-          <img src={rightArrow} alt="right arrow icon" className="w-12" />
+          <img src={rightArrow} alt="right arrow icon" className="w-8" />
         </button>
       </div>
     </section>
