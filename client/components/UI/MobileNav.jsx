98e6a05f5f51418b03dfccf271571f618f2aa24e
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-29 22:50:01 -0400

    navbar work in progress

--- client/components/UI/MobileNav.jsx
+++ client/components/UI/MobileNav.jsx
@@ -26,57 +26,67 @@ const MobileNav = ({ expand, setExpand }) => {
 
   return (
     <div
-      className="absolute top-0 z-10 mt-20 h-[calc(100vh-80px)] w-72 bg-gray-100 transition-all lg:hidden"
+      className="absolute top-0 left-0 z-50 h-screen  w-screen bg-[#344e41] font-gloock  text-white  transition-all md:hidden"
       style={{
         right: `${expand ? '0' : '-18rem'}`,
       }}
     >
-      <ul className="flex basis-16 flex-col items-center justify-center text-center text-2xl">
-        <div className="mx-auto flex h-16 w-5/6 gap-1 border-b-2 stroke-green-900 py-4">
-          <button onClick={handleSearch}>
-            <img src={searchIcon} alt="magnifying glass" className="w-8 pt-1" />
-          </button>
-          <form onSubmit={handleSearch}>
-            <input
-              type="text"
-              placeholder="succulent..."
-              className="h-8 w-full rounded-full border-2 border-green-700 bg-gray-100 pl-3 text-base"
-              value={searchTerm}
-              onChange={(e) => dispatch(adjustSearchBy(e.target.value))}
-            />
-          </form>
-        </div>
+      <ul className="flex  flex-col  pt-[50%] text-[6vw]">
         <div
-          className="w-full transition-all hover:bg-slate-200"
+          className="w-full text-center transition-all"
           onClick={handleClick}
         >
           {localStorage.getItem('token') ? (
             <Link to="/account">
-              <li className="mx-auto h-16 w-5/6 border-b-2 py-4">Account</li>
+              <li className="mx-auto max-h-16 w-full border-b pb-1 ">
+                <span className="self-start pr-2 text-[2vw]">01</span>ACCOUNT
+              </li>
             </Link>
           ) : (
             <Link to="/login">
-              <li className="mx-auto h-16 w-5/6 border-b-2 py-4">Login</li>
+              <li className=" mx-auto max-h-16 w-3/6 border-b  pb-1">
+                <span className="self-start pr-2 text-[2vw]">01</span>LOGIN
+              </li>
             </Link>
           )}
         </div>
         <div
-          className="w-full transition-all hover:bg-slate-200"
+          className="w-full text-center transition-all "
           onClick={handleClick}
         >
           <Link to="/products">
-            <li className="mx-auto h-16 w-5/6 border-b-2 py-4">Shop</li>
+            <li className="mx-auto max-h-16 w-full border-b pb-1 pt-2">
+              <span className="self-start pr-2 text-[2vw]">02</span>SHOP
+            </li>
           </Link>
         </div>
         <div
-          className="w-full transition-all hover:bg-slate-200"
+          className="w-full text-center transition-all"
           onClick={handleClick}
         >
           <Link to="/cart">
-            <li className="mx-auto h-16 w-5/6 border-b-2 py-4">Cart</li>
+            <li className="mx-auto max-h-16 w-full border-b pb-1 pt-2">
+              <span className="self-start pr-2 text-[2vw]">03</span> CART
+            </li>
           </Link>
         </div>
       </ul>
+
+      {/**search section */}
+      <div className="mx-auto flex h-16 w-5/6 gap-2 py-10">
+        <button onClick={handleSearch}>
+          <img src={searchIcon} alt="magnifying glass" className="w-8 pt-1" />
+        </button>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="succulent..."
+            className="h-8 w-full rounded-full  bg-gray-100 pl-3 text-base placeholder:text-[3.5vw] input:text-black"
+            value={searchTerm}
+            onChange={(e) => dispatch(adjustSearchBy(e.target.value))}
+          />
+        </form>
+      </div>
     </div>
   );
 };
