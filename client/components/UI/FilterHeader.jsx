b44ffc569811e2ccd683534d88a888e5a6391cfb
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-30 23:12:05 -0400

    change filter style

--- client/components/UI/FilterHeader.jsx
+++ client/components/UI/FilterHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FilterHeader = (props) => {
   return (
-    <h2 className="mb-2 text-2xl text-primary-button-green underline">
+    <h2 className="mb-2 mt-3 text-[3.9vw] md:text-[1.5vw] 3xl:text-[1.2vw] 4xl:text-[1vw] 5xl:text-[.8vw] text-white underline underline-offset-2 uppercase portrait:lg:text-[2.6vw]">
       {props.children}
     </h2>
   );
