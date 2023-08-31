417d688b3be013a068961975a9802b6b0df6420d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-30 23:09:29 -0400

    render 8 prods per view

--- client/components/AllProductsSection.jsx
+++ client/components/AllProductsSection.jsx
@@ -18,7 +18,7 @@ const AllProductsSection = () => {
 
   return (
     <main className="mx-6 mb-4 grid max-w-7xl grid-cols-1 justify-items-center gap-x-4 gap-y-8 xs:grid-cols-2 sm:mx-12 md:grid-cols-3 md:gap-x-8 lg:grid-cols-4">
-      {shownProducts?.slice(productPage, productPage + 8).map((product) => {
+      {shownProducts?.slice(productPage, productPage + 12).map((product) => {
         return <ProductCard product={product} key={product.id} />;
       })}
     </main>
