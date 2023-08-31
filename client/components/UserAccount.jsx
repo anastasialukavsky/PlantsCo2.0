a053ac6ba1265c00fc69197aaf0aadb80f6da473
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-31 18:14:22 -0400

    adj height

--- client/components/UserAccount.jsx
+++ client/components/UserAccount.jsx
@@ -36,12 +36,12 @@ const UserAccount = () => {
   if (!auth) return <h1>Page Loading...</h1>;
 
   return (
-    <div className="h-[calc(100vh_-_4rem)] lg:h-[calc(100dvh_-_82px)] w-screen bg-[url('/assets/misc_bg/acc5.webp')] bg-cover bg-center font-outfit text-green-gray ">
+    <div className="h-[calc(100dvh_-_5rem)] w-screen bg-[url('/assets/misc_bg/acc5.webp')] bg-cover bg-center font-outfit text-green-gray  md:h-[calc(100dvh_-_4rem)] lg:h-[calc(100dvh_-_82px)] xl:h-[calc(100dvh_-_100px)] 2xl:h-[calc(100dvh_-_105px)] 5xl:h-[calc(100dvh_-_159px)] 6xl:h-[calc(100dvh_-_200px)] portrait:lg:h-[calc(100dvh_-_5.9rem)]">
       <div className=" absolute top-16 left-1/2 mx-auto w-full max-w-sm -translate-x-1/2 pt-16 2xl:top-28 5xl:top-44 5xl:max-w-xl 6xl:top-64  ">
         <p className="font-extrabold pb-2 text-center text-4xl font-bold text-white">
           WELCOME BACK,
         </p>
-        <p className=" text-center md:text-3xl text-2xl font-xtralight uppercase text-white">
+        <p className=" text-center text-2xl font-xtralight uppercase text-white md:text-3xl">
           {auth.firstName}
         </p>
         <div className="mt-8 flex flex-col gap-3">
