e68bb440c1844732009ead06771cbe065bca02a8
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-31 11:19:05 -0400

    fix valid class field

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -25,7 +25,7 @@ export default function Signup() {
   });
 
   const validClass =
-    'appearance-none border portrait:w-72 w-96 3xl:py-2 py-3 px-4 text-gray-700 text-[3vw] md:text-[1.3vw] 5xl:text-[1vw] 6xl:text-[.8vw] leading-tight focus:outline-none focus:bg-white focus:shadow-outline';
+    'appearance-none border portrait:w-80 w-96 3xl:py-2 py-3 px-4 text-gray-700 text-[3vw] md:text-[1.3vw] 5xl:text-[1vw] 6xl:text-[.8vw] leading-tight focus:outline-none focus:bg-white focus:shadow-outline';
 
   const invalidClass =
     'appearance-none border portrait:w-72 border-red-700 text-[3vw] md:text-[1.3vw] 5xl:text-[1vw] 6xl:text-[.8vw] w-96 py-3 3xl:py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:shadow-outline';
@@ -77,13 +77,13 @@ export default function Signup() {
 
   return (
     <div className="right-0 bg-[url('/assets/misc_bg/login4.jpg')] bg-cover bg-center bg-no-repeat font-outfit text-white  md:h-[calc(100vh_-_10vh)] md:bg-[url('/assets/bg_img/login.jpg')] portrait:absolute portrait:top-0 portrait:mt-20 portrait:h-screen portrait:w-full portrait:lg:mt-20 ">
-      <div className="m-auto w-full max-w-sm pt-14 flex flex-col h-[80%] bg-pink-300">
-        <h2 className="text-center font-marcellus text-[7vw] md:text-[4vw] 3xl:text-[3vw] 5xl:text-[2vw]">
+      <div className="m-auto w-full max-w-sm xl:pt-16 pt-20 md:pt-5 ">
+        <h2 className="text-center font-marcellus text-[7vw] md:text-[3vw] 3xl:text-[3vw] 5xl:text-[2vw]">
           SIGN UP
         </h2>
         <section className="mt-2 flex flex-col items-center justify-center  gap-1">
-          <form onSubmit={handleSubmit} className="max-h-[70%]">
-            <div className="mb-2">
+          <form onSubmit={handleSubmit} className="">
+            <div className="lg:mb-1">
               <label className="mb-1 block text-sm " htmlFor="firstName">
                 First Name
               </label>
@@ -103,15 +103,15 @@ export default function Signup() {
               <p
                 className={
                   isInvalidFirstName
-                    ? 'mt-2 text-xs text-red-700'
-                    : 'collapse -mt-1'
+                    ? 'mt-1 text-xs text-red-700'
+                    : 'collapse '
                 }
               >
                 Please enter your first name!
               </p>
             </div>
 
-            <div className="mb-2">
+            <div className="lg:mb-1">
               <label className="mb-1 block text-sm " htmlFor="lastName">
                 Last Name
               </label>
@@ -131,15 +131,15 @@ export default function Signup() {
               <p
                 className={
                   isInvalidLastName
-                    ? 'mt-2 text-xs text-red-700'
-                    : 'collapse -mt-1'
+                    ? 'mt-1 text-xs text-red-700'
+                    : 'collapse '
                 }
               >
                 Please enter your last name!
               </p>
             </div>
 
-            <div className="mb-2">
+            <div className="lg:mb-1">
               <label className="mb-1 block text-sm " htmlFor="email">
                 Email
               </label>
@@ -158,15 +158,15 @@ export default function Signup() {
               <p
                 className={
                   isInvalidEmail
-                    ? 'mt-2 text-xs text-red-700'
-                    : 'collapse -mt-1'
+                    ? 'mt-1 text-xs text-red-700'
+                    : 'collapse '
                 }
               >
                 Please enter a valid email!
               </p>
             </div>
 
-            <div className="mb-2">
+            <div className="lg:mb-1">
               <label className="mb-1 block text-sm " htmlFor="password">
                 Password
               </label>
@@ -186,8 +186,8 @@ export default function Signup() {
               <p
                 className={
                   isInvalidPassword
-                    ? 'mt-2 text-xs text-red-700'
-                    : 'collapse -mt-1'
+                    ? 'mt-1 text-xs text-red-700'
+                    : 'collapse '
                 }
               >
                 Please enter a valid password (at least 8 chars)!
