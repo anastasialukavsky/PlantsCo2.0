1b9e51c83d0a1b28875e7424c2232214b7f58769
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-01 11:48:39 -0400

    add brpts text size for mobile

--- client/components/WishlistCard.jsx
+++ client/components/WishlistCard.jsx
@@ -20,31 +20,31 @@ const WishlistCard = ({ product }) => {
 
   return (
     <>
-      <div className="md:mx-4 flex h-52 items-center justify-around gap-6 portrait:xs:gap-3">
-        <div className="h-36">
+      <div className="flex h-52 items-center justify-around gap-6 md:mx-4 portrait:xs:gap-3 portrait:xs:px-1">
+        <div className="h-36 2xl:w-40">
           <img
             src={`${product.imageURL}`}
             alt={`product photo of ${product.name}`}
-            className="h-36"
+            className="h-36 2xl:w-40"
           />
         </div>
-        <div className=" min-w-48 flex flex-col items-center gap-2 align-top">
+        <div className=" min-w-48 flex flex-col  gap-2 align-top">
           <Link to={`/products/${product.id}`}>
-            <h1 className="cursor-pointer text-[1.5vw] uppercase hover:underline xl:text-[1.4vw] 2xl:text-[1.2vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:md:text-[2.5vw] text-center portrait:xs:text-[3.8vw]">
+            <h1 className="cursor-pointer text-[1.5vw] uppercase hover:underline xl:text-[1.4vw] 2xl:text-[1.2vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:xs:text-[3.8vw]  portrait:md:text-[2.5vw] portrait:text-center">
               {product.name}
             </h1>
           </Link>
-          <p className="text-[1.2vw] italic text-gray-600 xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-[1.8vw] portrait:md:text-[2vw] text-center portrait:xs:text-[2.7vw]">
+          <p className="portrait:text-center text-[1.2vw] italic text-gray-600 xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-[1.8vw] portrait:xs:text-[2.7vw] portrait:md:text-[2vw]">
             {product?.tags?.map(({ tagName }) => tagName).join(', ')}
           </p>
-          <p className="text-[1.5vw] xl:text-[1.3vw] 2xl:text-[1.1vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-[2.3vw] portrait:md:text-[2.5vw] portrait:xs:text-[3.2vw]">
+          <p className="text-[1.5vw] xl:text-[1.3vw] 2xl:text-[1.1vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-[2.3vw] portrait:xs:text-[3.2vw] portrait:md:text-[2.5vw] portrait:text-center">
             ${product.price}
           </p>
         </div>
         <div className="ml-8">
           <button
             onClick={() => handleRemove(product.id)}
-            className="block   border border-green-gray/30 portrait:xs:text-[3.1vw] py-1 px-2 text-[1.2vw] portrait:md:text-[2.2vw] portrait:text-[2vw] hover:bg-gray-100 3xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw]"
+            className="block   border border-green-gray/30 py-1 px-2 text-[1.2vw] transition-all duration-300 hover:bg-gray-100 3xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-[2vw] portrait:xs:text-[3.1vw] portrait:md:text-[2.2vw]"
           >
             remove
           </button>
