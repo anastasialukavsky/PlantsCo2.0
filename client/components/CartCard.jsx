2a6db6c338a032b51ed5f884c3247b04d2817641
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-31 23:24:19 -0400

    styled responsive

--- client/components/CartCard.jsx
+++ client/components/CartCard.jsx
@@ -30,7 +30,7 @@ const CartCard = (props) => {
 
   return (
     <>
-      <div className="mx-3 flex h-52 items-center justify-around gap-2 sm:mx-8 sm:gap-6 text-green-gray">
+      <div className="mx-3 flex h-52 items-center justify-around gap-2 text-green-gray sm:mx-8 sm:gap-6">
         <div className="h-36">
           <img
             src={`${product.imageURL}`}
@@ -38,15 +38,17 @@ const CartCard = (props) => {
             className="h-36"
           />
         </div>
-        <div className="flex w-48 md:w-56 flex-col gap-3">
+        <div className="flex w-48 flex-col gap-3 md:w-56">
           <Link to={`/products/${product.id}`}>
-            <h1 className="text-xl hover:underline uppercase">{product.name}</h1>
+            <h1 className="text-xl uppercase hover:underline">
+              {product.name}
+            </h1>
           </Link>
           <p className="text-xs italic text-gray-600">
             {product?.tags.map(({ tagName }) => tagName).join(', ')}
           </p>
           <p>${product.price}</p>
-          <div className="flex gap-2 w-fit border border-green-gray rounded-full px-2">
+          <div className="flex w-fit gap-2 rounded-full border border-green-gray px-2">
             <button onClick={() => decrementCart(product.id)}>
               <img src={minus} alt="minus qty icon" className="w-5" />
             </button>
@@ -56,12 +58,12 @@ const CartCard = (props) => {
             </button>
           </div>
         </div>
-        <div className="sm:ml-8">
+        <div className="">
           <button
             onClick={() => {
               removeFromCart(product.id);
             }}
-            className="block  py-1 hover:bg-gray-200 sm:rounded border border-green-gray/30 px-2"
+            className="block  border border-green-gray/30 py-1 px-2   hover:bg-gray-200"
           >
             remove
           </button>
