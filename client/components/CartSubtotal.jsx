fbcaa74aceebe5baca8631f2b1ce8869ee39c8f3
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-01 12:48:51 -0400

    style adj

--- client/components/CartSubtotal.jsx
+++ client/components/CartSubtotal.jsx
@@ -7,8 +7,8 @@ const CartSubtotal = () => {
   const subtotal = useSelector(selectCartSubtotal);
 
   return (
-    <div className="">
-      <h2 className="ml-8 text-[1.6vw] 6xl:text-[.9vw] 3xl:text-[1.4vw] 5xl:text-[1.2vw] pt-2 text-center portrait:xs:text-[4vw]">SUBTOTAL: {subtotal.toFixed(2)}</h2>
+    <div className="pt-3">
+      <h2 className=" portrait:md:text-[2.7vw] text-[1.6vw] 6xl:text-[.7vw] 3xl:text-[1.4vw] 5xl:text-[.8vw] pt-2 text-center portrait:xs:text-[4vw]">SUBTOTAL: {subtotal.toFixed(2)}</h2>
     </div>
   );
 };
